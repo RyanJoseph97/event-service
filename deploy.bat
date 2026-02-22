b0ffbf658c368caa62344291491fbca46508364b
Ryan Joseph <ryanjosephjr97@gmail.com>
2026-02-22 13:25:51 -0600

    Migrating application from spring to springboot

--- deploy.bat
+++ deploy.bat
@@ -1,27 +1,14 @@
 @echo off
-set WEBAPPS_PATH=C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\
 
 echo Running Maven build...
-call mvn clean install
+call mvn clean package
 if %ERRORLEVEL% NEQ 0 (
     echo Maven build failed. Exiting.
     exit /b %ERRORLEVEL%
 )
 
-echo Moving files to webapps directory...
+echo Starting event-service...
+cd target
+java -jar event-service-1.0-SNAPSHOT.jar
 
-if exist target\event-service.war (
-    move /Y target\event-service.war "%WEBAPPS_PATH%"
-) else (
-    echo event-service.war not found!
-)
-
-if exist target\event-service (
-    xcopy /E /I /Y "target\event-service" "%WEBAPPS_PATH%\event-service"
-) else (
-    echo event-service directory not found!
-)
-
-echo Done moving event-service files
-
-call catalina.bat jpda start
+echo event-service started successfully
